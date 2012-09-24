786389478196286de6b34c669c5dc6a82837f62a
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-09-24 14:08:13 +0200

    version increment

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "sebgrewe@bigpoint.net"
 license          "All rights reserved"
 description      "Installs/Configures icinga"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.22"
+version          "0.1.23"
 
 depends "build-essential", ">= 1.1.2"
