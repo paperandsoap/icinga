108a9cbc578e62510c1a726245755fc7005eca5b
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-09-24 12:26:24 +0200

    one last attempt, then I seek help

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "sebgrewe@bigpoint.net"
 license          "All rights reserved"
 description      "Installs/Configures icinga"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.15"
+version          "0.1.16"
 
 depends "build-essential", ">= 1.1.2"
