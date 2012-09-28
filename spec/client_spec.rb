33e7a5b748b8f1073a1e8eaed646e7fe5d5fe91d
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-09-28 15:46:17 +0200

    added more comments and cleaned up idents

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,17 +8,20 @@ require 'chefspec'
       @chef_run.converge 'icinga::client'
     }
 
+    # Check all packages that are installed
     %w( xinetd ethtool check-mk-agent check-mk-agent-logwatch ).each do |pkg|
       it "should install #{pkg}" do
         @chef_run.should install_package pkg
       end
     end
-    it "should start xinetd on boot" do
+
+    # Check that our xinetd service is enabled and running
+    it "should start xinetd and enabled it boot" do
       @chef_run.should set_service_to_start_on_boot 'xinetd'
-    end
-    it "should start xinetd" do
       @chef_run.should start_service 'xinetd'
     end
+
+    # Check that our template file is created to enable the check_mk agent
     it "should create file /etc/xinet.d/check_mk" do
       @chef_run.should create_file "/etc/xinetd.d/check_mk"
     end
