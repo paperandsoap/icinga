0b1c790b8908bbf580e6639423d628f83fa7961a
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-11-28 13:16:33 +0100

    removed unneeded dependencies or platform based dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.1.37"
 
 recipe "icinga", "Icinga Client Monitoring"
-recipe "icinga::server", "Icinga Monitoring Server and node discovery for the server node environment"
-recipe "icinga::master", "Icinga Monitoring Server and Master with monitoring-server role discovery"
+recipe "icinga::server", "Icinga Monitoring Server and configurable monitoring node discovery"
+recipe "icinga::master", "Icinga Monitoring Server and Master Setup with configurable role discovery"
 
 # Default settings used in configuration options below
 attribute "pnp4nagios/config/rrdbase",
@@ -67,4 +67,3 @@ attribute "check_mk/groups",
 
 depends "build-essential", ">= 1.1.2"
 depends "apache2", ">= 1.1.16"
-depends "apt", ">= 1.4.8"
