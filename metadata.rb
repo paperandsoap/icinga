1ae30f70f5c6dfc33675f879e09cf03901aa41ed
Sebastian Grewe <sebastian@grewe.ca>
2012-11-30 09:25:27 +0100

    Update metadata.rb
    Changed license.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer "BigPoint GmbH"
 maintainer_email "sebgrewe@bigpoint.net"
-license "All rights reserved"
+license "Apache 2.0"
 description "Installs/Configures Icinga and check_mk Monitoring Solution"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.1.40"
