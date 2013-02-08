dcd0582b576aa74e75267a794c4732c6385ef48d
jenkins <svc_hudson@bigpoint.net>
2013-02-08 11:31:23 +0000

    increase version number to 0.1.67

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sebgrewe@bigpoint.net'
 license          'Apache 2.0'
 description      'Installs/Configures Icinga and check_mk Monitoring Solution'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.66"
+version           "0.1.67"
 
 recipe 'icinga', 'Icinga Client Monitoring'
 recipe 'icinga::server', 'Icinga Monitoring Server and configurable monitoring node discovery'
